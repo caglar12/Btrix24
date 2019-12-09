c4981e94aa495baa93d985a375cd3ae0c3ed3570
caglar12 <54274519+caglar12@users.noreply.github.com>
2019-12-09 19:18:24 +0100

    added return method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,8 +1,13 @@
 package tests;
 
 public class task {
+  
   public static void main (String[] arg){
   System.out.println("Hello world!");
     //this line is added from Github, see you ion InteliJ
+  }
+  
+  public String hello(){
+  return"Hello from Github";
   }
 }
